707e0b0121c3a3f8a9c0a0b2b0d4756f7d75e821
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-07-19 17:46:49 +0300

    Initial commit

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]):T {
+  return arr[0];
+}
+
+console.log(getFirstElement([1, 2, 3]));           // 1
+console.log(getFirstElement(["a", "b", "c"]));     // "a"
+console.log(getFirstElement([true, false, true])); // true
+
+// console.log(getFirstElement<number>([true, "false", 6]));
